0c4bee6507bdbbb1732b162ed63e307fd03f9d9c
wberg <wberg@wberg.com>
2026-05-31 21:06:14 +0200

    change agent

--- worker/src/bordershop.ts
+++ worker/src/bordershop.ts
@@ -27,7 +27,7 @@ export interface BordershopCategoryResponse {
 }
 
 const USER_AGENT =
-  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36";
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36";
 
 export async function getCategory(
   categoryId: number,
